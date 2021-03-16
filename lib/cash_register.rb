17566c32d3c501240cb40fe83532877708d6628d
alexandru <boncut.alex10@gmail.com>
2021-03-16 16:34:33 -0500

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,47 @@
+class CashRegister
+    attr_accessor :total, :discount, :price
+    
+  
+    def initialize(discount = 0)
+      @total = 0
+      @discount = discount
+      @cart = []
+    end
+  
+    def add_item(item, price, quantity = 1)
+      # Need the price variable to keep track of the cost for the last item added
+      @price = price * quantity
+      cart2 = {}
+      cart2[:name] = item
+      cart2[:price] = price
+      cart2[:quantity] = quantity
+  
+      @cart << cart2
+  
+      @total += price * quantity
+  
+    end
+  
+    def apply_discount
+      if @discount == 0
+        return "There is no discount to apply."
+      end
+      @total -= @total * @discount / 100
+      return "After the discount, the total comes to $#{@total}."
+    end 
+  
+    def items
+      item_names = []
+      @cart.each do |cart2|
+        for each_item in 1..cart2[:quantity] 
+          item_names << cart2[:name]
+        end 
+      end 
+      item_names
+    end 
+  
+    def void_last_transaction
+      @total -= @price
+    end
 
+  end
