178a269553df4aaa70b44fcecde0126869a7486c
alexandru <boncut.alex10@gmail.com>
2021-03-16 16:36:26 -0500

    commit tree 6b10e26c5b4f75062f79bbc56aef7fac097b239d

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,45 +3,45 @@ class CashRegister
     
   
     def initialize(discount = 0)
-      @total = 0
-      @discount = discount
-      @cart = []
+        @total = 0
+        @discount = discount
+        @cart = []
     end
   
     def add_item(item, price, quantity = 1)
-      # Need the price variable to keep track of the cost for the last item added
-      @price = price * quantity
-      cart2 = {}
-      cart2[:name] = item
-      cart2[:price] = price
-      cart2[:quantity] = quantity
+        # Need the price variable to keep track of the cost for the last item added
+        @price = price * quantity
+        cart2 = {}
+        cart2[:name] = item
+        cart2[:price] = price
+        cart2[:quantity] = quantity
   
-      @cart << cart2
+        @cart << cart2
   
-      @total += price * quantity
+        @total += price * quantity
   
     end
   
     def apply_discount
-      if @discount == 0
-        return "There is no discount to apply."
-      end
-      @total -= @total * @discount / 100
-      return "After the discount, the total comes to $#{@total}."
+        if @discount == 0
+            return "There is no discount to apply."
+        end
+        @total -= @total * @discount / 100
+        return "After the discount, the total comes to $#{@total}."
     end 
   
     def items
-      item_names = []
-      @cart.each do |cart2|
-        for each_item in 1..cart2[:quantity] 
-          item_names << cart2[:name]
+        item_names = []
+        @cart.each do |cart2|
+            for each_item in 1..cart2[:quantity] 
+                item_names << cart2[:name]
+            end 
         end 
-      end 
-      item_names
+        item_names
     end 
   
     def void_last_transaction
-      @total -= @price
+        @total -= @price
     end
 
   end
